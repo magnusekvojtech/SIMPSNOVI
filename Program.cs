9e1ae1a647846dc5008754fef39081f98cd53f4d
√ｲﾶﾑム <113216701+magnusekvojtech@users.noreply.github.com>
2023-02-23 22:41:07 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace simpsonovi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] simpsonovi = { "Homer", "Marge", "Bart", "Lisa", "Magie" };
+            Array.Sort(simpsonovi);
+            foreach (string serazeno in simpsonovi)
+                Console.WriteLine("{0}", serazeno);
+            Console.ReadKey();
+        }
+    }
+}
